8e26f945ffd5c62a3d0e6e0c8fe6856261175fbe
Critteros <szymonkoziol3@gmail.com>
2021-01-03 15:08:18 +0100

    Added resizing array and putting element to an array support

--- StringContainer.c
+++ StringContainer.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#pragma warning (disable: 4996 6001 6387 )
 
-
+#include <string.h>
+#include <stdlib.h>
 /////////////////////////////////////////////////////////////////////////////////////////////
 //StringContainer is a dynamic array list for C strings									   //
 //Written by Szymon Kozio³ (Critteros) for C labs exercises								   //
@@ -20,14 +22,76 @@ typedef struct StringContainer strings;
 strings NewConatiner(void); //Return new initialized struct for use 
 void DeleteContainer(strings* obj); //Deletes struct
 void strings_put(strings* obj, char* string); //Puts a C string to an array
-char* strings_get(strings* obj, size_t index); //Returns an element from an array of specific index
-char* strings_remove(strings* obj, size_t index); //Removes element of specific index from an array
-void strings_clear(strings* obj); //Clears the array
+//char* strings_get(strings* obj, size_t index); //Returns an element from an array of specific index
+//char* strings_remove(strings* obj, size_t index); //Removes element of specific index from an array
+//void strings_clear(strings* obj); //Clears the array
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+strings NewConatiner(void)
+{
+	strings context;
+	const size_t initialCapacity = 1;
+
+	context.capacity = initialCapacity;
+	context.m_Data = (char**)malloc(initialCapacity * sizeof(char*));
+	for (int i = 0; i < initialCapacity; i++)
+	{
+		context.m_Data[i] = NULL;
+	}
+	context.size = 0;
+
+	return context;
+}
+
+void DeleteContainer(strings* obj)
+{
+	for (size_t i = 0; i < obj->capacity; i++)
+	{
+		if (obj->m_Data[i])
+		{
+			free(obj->m_Data[i]);
+		}
+	}
+	free(obj->m_Data);
+}
+
+void strings_put(strings* obj, char* string)
+{
+	if (obj->capacity == obj->size)
+	{
+		//Time to resize
+		size_t newSize = obj->capacity * 2;
+		obj->m_Data = (char**)realloc(obj->m_Data, newSize * sizeof(char*));
+		obj->capacity = newSize;
+	}
+
+	size_t putIndex = obj->size;
+	size_t neededSize = strlen(string) + 1;
+	char* space = (char*)malloc(neededSize * sizeof(char));
+	strcpy(space, string);
+	obj->m_Data[putIndex] = space;
+	obj->size++;
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
-
+//For testing only
 int main(void)
 {
-	printf("Hello World\n");
+	strings test = NewConatiner();
+	strings_put(&test, "Aba");
+	strings_put(&test, "skakdkasmdkasdm");
+	strings_put(&test, "skasadasdasdas");
+	strings_put(&test, "skakdkasdasdasdkasdm");
+
+
+	
+	printf("%s\n", test.m_Data[0]);
+	printf("%s\n", test.m_Data[1]);
+
+	DeleteContainer(&test);
+	return 0;
 }
